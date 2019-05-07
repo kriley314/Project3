12755c030553650608a1b3e623f773ad5c22c9a7
Derek <45375006+derekgeorg@users.noreply.github.com>
2019-05-06 22:20:13 -0500

    app is deploying on localhost:3000

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo_only.png';
+import Navbar from "./components/Navbar";
+import MapBox from "./components/MapBox";
+import UserListBox from "./components/UserListBox";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>This works if you see this.</h1>
     </div>
   );
 }
