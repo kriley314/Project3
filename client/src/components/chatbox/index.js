a74476ee08f91dae8498afeb570bee6a629cee1a
bbierman <bbierman7@gmail.com>
2019-05-10 22:48:07 -0500

    added chat component

--- client/src/components/chatbox/index.js
+++ client/src/components/chatbox/index.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import firebase from "../../utils/firebase.js";
+import API from "../../utils/API";
 
-import firebase from "./utils/firebase.js";
 
-import './App.css';
+class ChatBox extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props.messagesArray)
+    this.state = { 
+        messagesArray: this.props.messagesArray
+    };
+  }
 
 
-componentDidMount() {
-    // var ref = firebase.database().ref("/chat");
-    // ref.once("value")
-    //   .then(function (snapshot) {
-    //     console.log(snapshot.val());
-    //   });
+  componentDidMount() {
     firebase.database().ref("/chat").orderByChild("time").on("child_added", snapshot => {
-
-      console.log("Snapshot: ",snapshot.val());
+      console.log("Snapshot: ", snapshot.val());
+      console.log('messaes', this.state.messagesArray)
       const newMessagesArray = this.state.messagesArray;
       newMessagesArray.push(snapshot.val());
-
       this.setState({ messagesArray: newMessagesArray });
     })
-  };
+    this.loadUsers();
+  }
 
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
+  };
+
+  loadUsers = () => {
+    API.getUsers()
+      .then(res =>
+        this.setState({ users: res.data, name: "", id: "", },() => console.log(res.data))
+      )
+      .catch(err => console.log(err));
   };
 
   chatSubmit = event => {
@@ -33,19 +45,30 @@ componentDidMount() {
       message: this.state.chatText,
       time: firebase.database.ServerValue.TIMESTAMP
     })
+    
+    this.setState({chatText: ""})
   }
 
 
+  render() {
+    return (
+      <div>
+        <form>
+          <div className="form-group">
+            <button onClick={this.chatSubmit} style={{ marginBottom: 10 }} type="button" class="btn btn-dark">Chat <i className="far fa-comment-alt" style={{ marginLeft: 5 }}></i></button>
 
-  <form>
-  <div className="form-group">
-    <button onClick={this.chatSubmit} style={{ marginBottom: 10 }} type="button" class="btn btn-dark">Chat <i className="far fa-comment-alt" style={{ marginLeft: 5 }}></i></button>
+            <textarea style={{ padding: 10 }} onChange={this.handleInputChange} name="chatText" className="form-control" id="exampleFormControlTextarea1" rows="3" value={this.state.chatText}></textarea>
+          </div>
+        </form>
 
-    <textarea style={{ padding: 10 }} onChange={this.handleInputChange} name="chatText" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-</form>
-<div className="messageContainer">
-  {this.state.messagesArray.map(messageObj =>
-    <p>{messageObj.name} said: "{messageObj.message}"</p>
-  )}
-</div>
+        <div className="messageContainer">
+          {this.state.messagesArray.map(messageObj =>
+            <p>{messageObj.name} said: "{messageObj.message}"</p>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ChatBox;
